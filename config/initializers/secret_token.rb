71214a0ba29d21490a601752a8d727923783933d
unknown <ebauer@FOLMABL1383.corp.foliage.com>
2012-09-25 22:47:55 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baueric::Application.config.secret_token = 'd1dd61867950b817cdfe640a2090441c37cbe3c7e9fa30872b1ea3303fd25d090d2bc22bb0a18e53826c9c4dac95c461060ea1c1775364132c200be09c83ec5a'
